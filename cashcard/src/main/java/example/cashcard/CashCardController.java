40a92d08a76b72f6932c4c6113ec3c0b6a5909d4
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2025-08-17 08:39:25 -0300

    feat: implement pagination in the endpoint and repository

--- cashcard/src/main/java/example/cashcard/CashCardController.java
+++ cashcard/src/main/java/example/cashcard/CashCardController.java
@@ -1,7 +1,12 @@
 package example.cashcard;
 
 import java.util.Optional;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +48,8 @@ public class CashCardController {
     }
 
     @GetMapping()
-    private ResponseEntity<Iterable<CashCard>> findAll() {
-        return ResponseEntity.ok(cashCardRepository.findAll());
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()));
+        return ResponseEntity.ok(page.getContent());
     }
 }
--- cashcard/src/main/java/example/cashcard/CashCardRepository.java
+++ cashcard/src/main/java/example/cashcard/CashCardRepository.java
@@ -1,8 +1,9 @@
 package example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
     // This interface will automatically provide CRUD operations for CashCard entities
     // No additional methods are needed unless custom queries are required
 }
