f54c9a3eb499a4e9d3343b939e0d5339344961de
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2025-08-18 09:46:41 -0300

    feat: get cash card list orderly

--- cashcard/src/main/java/example/cashcard/CashCardController.java
+++ cashcard/src/main/java/example/cashcard/CashCardController.java
@@ -49,7 +49,7 @@ public class CashCardController {
 
     @GetMapping()
     private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
-        Page<CashCard> page = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()));
+        Page<CashCard> page = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort()));
         return ResponseEntity.ok(page.getContent());
     }
 }
